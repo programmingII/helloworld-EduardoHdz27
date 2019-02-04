a76e97233c28579533cb7ab33b6db23bbcdd6ba9
Eduardo Hernández <al163984@alumnos.uacj.mx>
2019-02-04 15:02:49 -0700

    Update HelloWorld.java

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -7,7 +7,7 @@ package helloworld;
 
 /**
  *
- * @author user
+ * @EduardO Hdz
  */
 public class HelloWorld {
 
@@ -16,8 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello, World");
-        System.out.println("Happy coding");
+        System.out.println("Hello World!\n My name is: Eduardo Hdz\n and my matriculation number is: 163984\n");
     }
     
 }
